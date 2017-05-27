77905483ae5fc89682ad75328527c9a6f295f040
Joshua Pratt <jp10010101010000@gmail.com>
2017-05-27 18:14:03 +1000

    Moved game logic into bot and made a decorator to use the bot

--- app/qa_bot.py
+++ app/qa_bot.py
@@ -2,29 +2,49 @@
     defines a 20Qs bot
 """
 from random import choice
-from app.db import Question #, Animal
+import sqlalchemy
+from app.db import Question, add_game #, Animal
 
-NUM_QUESTIONS=20
+NUM_QUESTIONS = 20
 
 class QaBot(object):
     """
-        Takes a dataset and a set of answered questions
+        Takes a dataset and a set of questions questions
         Makes guesses about good questions to ask
         and possible solutions
     """
-    def __init__(self, answered):
-        self.answered = answered
+    def __init__(self, serialized=None):
+        self.questions = []
+        self.guesses = None
+        if serialized is not None:
+            self.__dict__.update(serialized)
+
+    def finish_game(self, solution):
+        if solution is not None:
+            # save solution and questions to data base
+            print("ADD GAME ", solution, self.questions)
+            add_game(solution, self.questions)
+
+    def get_guesses(self):
+        if self.guesses is not None:
+            return self.guesses
+        guesses = [("kangaroo", 0.5), ("dog", 0.9), ("rabbit", 0.1), ("octopus", 0.01)]
+        self.guesses = guesses
+        return sorted(guesses, key=lambda x: -x[1])
 
     def get_question(self):
-        questions_asked = []
+        """ Return the next question to ask """
+        if self.guesses is None:
+            self.get_guesses()
+
         question_query = []
         try:
             new_questions = Question.query # .filter(~Question.id.in_(questions_asked))
 
             # filter / order and limit to get maximal split
             question_query = new_questions.all()
-        except Exception as error:
-            print(error)
+        except sqlalchemy.exc.OperationalError as error:
+            print("SQLALCHEMY ERROR: ", error)
 
         if question_query == []:
             return ("Sorry, I don't know this one", [])
@@ -41,9 +61,23 @@ class QaBot(object):
         return (q_txt, options)
 
     def give_answer(self, question, answer):
-        print('Q: {}'.format(question))
-        print('A: {}'.format(answer))
+        if question and answer:
+            print('Q: {}'.format(question))
+            print('A: {}'.format(answer))
+            self.questions.append((question, answer))
+            self.guesses = None # Reset
+        return self.questions
 
-    def guesses(self):
-        guesses = [("kangaroo", 0.5), ("dog", 0.9), ("rabbit", 0.1), ("octopus", 0.01)]
-        return sorted(guesses, key=lambda x: -x[1])
+    def undo(self):
+        if self.questions != []:
+            self.questions.pop()
+            self.guesses = None
+
+    def question_number(self):
+        return len(self.questions)+1
+
+    def game_finished(self):
+        return self.question_number() > NUM_QUESTIONS
+
+    def serialize(self):
+        return self.__dict__
--- app/routes.py
+++ app/routes.py
@@ -4,76 +4,80 @@
 from flask import render_template, session, request, url_for, redirect
 #, flash, Markup
 from app.server import app, cache, DEFAULT_TIMEOUT
+from app.qa_bot import QaBot
 
-from app.qa_bot import QaBot, NUM_QUESTIONS
-from app.db import add_game
-
-@app.route('/')
-def new_game():
-    session['questions'] = {}
-    session['question_number'] = 1
-    return render_template('new_game.html')
+def with_bot(func):
+    name = func.__name__
+    def with_bot_wrapper(*args, **kwargs):
+        bot = QaBot(session.get('bot'))
+        r_value = func(bot, *args, **kwargs)
+        session['bot'] = bot.serialize()
+        return r_value
+    with_bot_wrapper.__name__ = name
+    return with_bot_wrapper
 
 @app.route('/about')
 @cache.cached(timeout=DEFAULT_TIMEOUT)
 def about():
     return render_template('about.html')
 
+@app.route('/')
+@with_bot
+def new_game(bot):
+    bot.__init__() # clear the bot
+    return render_template('new_game.html')
+
 @app.route('/question/')
-def question():
-    # display a question and a set of guesses
-    print(session.get('questions', 'No questions'))
-    bot = QaBot([])
+@with_bot
+def question(bot):
+    """
+    Gets a set of guesses and a question from the bot
+    and allows the user to answer it or go back
+    """
+    action = request.args.get('action')
+    if action == 'back':
+        bot.undo()
+
+    print("Qs: ", bot.questions)
     question_txt, options = bot.get_question()
-    guesses = bot.guesses()
     return render_template(
         'question.html',
+        question_number=bot.question_number(),
         question=question_txt,
-        question_number=session.get('question_number', 1),
         options=options,
-        guesses=guesses
+        guesses=bot.get_guesses()
     )
 
 @app.route('/answer')
-def answer():
-    session['question_number'] = session.get('question_number', 0)+1
+@with_bot
+def answer(bot):
+    """ Take an answer from the player """
     # receive an answer from a user
-    bot = QaBot(session['questions'])
-    question_txt = request.args.get('question')
-    answer_txt = request.args.get('answer')
+    bot.give_answer(
+        request.args.get('question'),
+        request.args.get('answer')
+    )
+    session['bot'] = bot.serialize()
 
-    if question_txt and answer_txt:
-        bot.give_answer(question_txt, answer_txt)
-        questions = session.get('questions')
-        if not questions:
-            questions = {}
-        questions[question_txt] = answer_txt
-        session['questions'] = questions
-    else:
-        print("Did not receive an answer")
-
-    if session['question_number'] > NUM_QUESTIONS:
+    if bot.game_finished():
         return redirect(url_for('guess'))
     return redirect(url_for('question'))
 
+@with_bot
 @app.route('/guess')
-def guess():
-    bot = QaBot(session.get('questions', {}))
-    guesses = bot.guesses()
+def guess(bot):
     return render_template(
         'guess.html',
-        guesses=guesses
+        guesses=bot.get_guesses()
     )
 
+@with_bot
 @app.route('/feedback/', defaults={'solution': None}, methods=['GET', 'POST'])
 @app.route('/feedback/<solution>', methods=['GET', 'POST'])
-def feedback(solution):
-    if solution is None:
-        solution = request.form.get('solution')
-    if solution is not None:
-        # save solution and questions to data base
-        questions = session.get('questions', {})
-        if questions != {}:
-            print("ADD GAME ", solution, questions)
-            add_game(solution, questions)
+def feedback(bot, solution):
+    solution = request.form.get(
+        'solution',
+        solution
+    )
+    bot.finish_game(solution)
     return redirect(url_for('new_game'))
