0183411d0e3526ca79a56647d2c4a3e62deb3e11
Joshua Pratt <jp10010101010000@gmail.com>
2017-05-28 14:50:57 +1000

    Added decent guessing (doesn't use how common the animal is but uses the answers)

--- app/qa_bot.py
+++ app/qa_bot.py
@@ -3,10 +3,10 @@
 """
 from random import choice
 import sqlalchemy
-from sqlalchemy.ext.serializer import loads, dumps
 from .db import Question, Animal, Entry, add_game
 
 NUM_QUESTIONS = 20
+ANSWERS = ['Yes', 'No', 'Unsure']
 
 class QaBot(object):
     """
@@ -18,45 +18,42 @@ class QaBot(object):
         self.questions = []
         self.guesses = None
         if serialized is not None:
-            self.__dict__.update(serialized)
-            self.guesses = loads(self.guesses)
+            self.questions = serialized['questions']
+            if 'guesses' in serialized:
+                try:
+                    self.guesses = []
+                    for (name, _id, prob) in serialized['guesses']:
+                        animal = Animal(name)
+                        animal.id = _id
+                        animal.prob = prob
+                        self.guesses.append(animal)
+                except ValueError:
+                    print("CORRUPTED")
 
     def serialize(self):
-        return {
-            'questions': self.questions,
-            'guesses': dumps(self.guesses)
-        }
+        out = {'questions': self.questions}
+        if self.guesses is not None:
+            out['guesses'] = [(guess.name, guess.id, guess.prob) for guess in self.guesses]
+        return out
 
     def get_guesses(self):
         """
         Get a weighted list of animals that 'best' fit our information
         """
         if self.guesses is not None:
-            print("CACHED GUESSES")
             return self.guesses
         print("RECALCULATING GUESSES")
-        animals = []
+        self.guesses = []
         try:
             animals_query = Animal.query
-            animals = animals_query.all()
-            for animal in animals:
-                animal.prob = animal.count
-
-            for animal in animals:
-                for question, answer in self.questions:
-                    entries = Entry.query\
-                            .filter(Entry.animal == animal)\
-                            .filter(Entry.question.has(question=question))
-                    responses = [ent.answer for ent in entries.all()] + ['Yes', 'No', 'Unsure']
-                    responses = {resp:responses.count(resp) for resp in responses}
-                    animal.prob *= responses[answer] / sum(responses.values())
+            self.guesses = animals_query.all()
+            for animal in self.guesses:
+                animal.prob = 1 # animal.count
         except sqlalchemy.exc.OperationalError as error:
             print("SQLALCHEMY ERROR: ", error)
 
-        total_plays = sum([animal.prob for animal in animals])
-        for animal in animals:
-            animal.prob /= total_plays
-        self.guesses = sorted(animals, key=lambda animal: -animal.prob)
+        for question, answer in self.questions:
+            self.guesses = adjust_guesses(self.guesses, question, answer)
         return self.guesses
 
     def get_question(self):
@@ -66,7 +63,7 @@ class QaBot(object):
 
         question_query = []
         try:
-            new_questions = Question.query # .filter(~Question.id.in_(questions_asked))
+            new_questions = Question.query.filter(~Question.id.in_([q[0] for q in self.questions]))
 
             # filter / order and limit to get maximal split
             question_query = new_questions.all()
@@ -77,20 +74,16 @@ class QaBot(object):
             return ("Sorry, I don't know this one", [])
 
         question = choice(question_query)
-        options = ['Yes', 'No', 'Unsure']
-
-        # print('Q: {}'.format(question))
-        # print('A: {}'.format(options))
 
         q_txt = question.question
         if q_txt[-1] != '?':
             q_txt += '?'
-        return (q_txt, options)
+        return (q_txt, ANSWERS)
 
     def give_answer(self, question, answer):
         if question and answer:
             self.questions.append((question, answer))
-            self.guesses = None # Reset
+            self.guesses = adjust_guesses(self.get_guesses(), question, answer)
         return self.questions
 
     def finish_game(self, solution):
@@ -102,10 +95,34 @@ class QaBot(object):
     def undo(self):
         if self.questions != []:
             self.questions.pop()
-            self.guesses = None
+            self.guesses = None # will be recaluated
 
     def question_number(self):
         return len(self.questions)+1
 
     def game_finished(self):
         return self.question_number() > NUM_QUESTIONS
+
+
+def adjust_guesses(animals, question, answer):
+    """
+    Takes a set of guesses and applies a question's answer probability distribution
+    """
+    try:
+        for animal in animals:
+            entries = Entry.query\
+                    .filter(Entry.animal == animal)\
+                    .filter(Entry.question.has(question=question))
+            responses = [ent.answer for ent in entries.all()] + ANSWERS
+            responses = {resp:responses.count(resp) for resp in responses}
+            animal.prob *= responses[answer] / sum(responses.values())
+    except sqlalchemy.exc.OperationalError as error:
+        print("SQLALCHEMY ERROR: ", error)
+
+    total_plays = sum([animal.prob for animal in animals])
+    if total_plays != 0:
+        for animal in animals:
+            animal.prob /= total_plays
+
+    animals = sorted(animals, key=lambda animal: -animal.prob)
+    return animals
